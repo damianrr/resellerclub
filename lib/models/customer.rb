96a84e357bde0893810c2ce9ad198ad55b37b1d0
Damián Rodríguez Rivero <damianrr@gmail.com>
2012-10-30 11:43:05 -0500

    getting update in alpha

--- lib/models/customer.rb
+++ lib/models/customer.rb
@@ -9,30 +9,6 @@ class Customer
   @@url_update = "modify.json"
   @@auth_userid = "430162"
   @@auth_password = "myresellerpass"
-  @@values = {
-    "auth_userid" => @@auth_userid,
-    "auth_password" => @@auth_password,
-    "username" => "",         # Should be an email address, required
-    "passwd" => "",           # Should be alphanumeric with a minimum of 8 and maximum of 16 characters. Required
-    "name" => "",             # Required
-    "company" => "",          # Required
-    "address_line_1" => "",   # Required
-    "address_line_2" => "",
-    "address_line_3" => "",
-    "city" => "",             # Required
-    "state" => "",            # Required
-    "country" => "",          # Country Code as per ISO 3166-1 alpha-2.
-    "zipcode" => "",          # Required
-    "phone" => "",            # Required
-    "alt_phone" => "",
-    "alt_phone_cc" => "",     # Alternate phone Country Code.
-    "phone_cc" => "",         # Telephone number Country Code. Required
-    "lang_pref" => "en",      # Language Code as per ISO. Required
-    "fax" => "",
-    "fax_cc" => "",           # Fax country code.
-    "mobile" => "",
-    "mobile_cc" => "",        # Mobile country code.
-  }
   class << self
     def construct_url(params, method)
       m = case method
@@ -51,21 +27,80 @@ class Customer
     end
 
     def validate(params)
-      True
+      true
     end
 
     def add(params={})
       # Example of use: Customer.add({"username" => "tyler@google.com","passwd" => "tambi5en5","name" => "Tyler","address_line_1" => "56 st pensilvania", "company" => "Transmeta", "city" => "Lisa", "state" => "CH", "country" => "CU", "zipcode" => "11155", "phone_cc" => "53", "phone" => "531531"})
-      values = @@values.merge!(params)
+      values = {
+        "auth_userid" => @@auth_userid,
+        "auth_password" => @@auth_password,
+        "username" => "",         # Should be an email address, required
+        "passwd" => "",           # Should be alphanumeric with a minimum of 8 and maximum of 16 characters. Required
+        "name" => "",             # Required
+        "company" => "",          # Required
+        "address_line_1" => "",   # Required
+        "address_line_2" => "",
+        "address_line_3" => "",
+        "city" => "",             # Required
+        "state" => "",            # Required
+        "country" => "",          # Country Code as per ISO 3166-1 alpha-2.
+        "zipcode" => "",          # Required
+        "phone" => "",            # Required
+        "alt_phone" => "",
+        "alt_phone_cc" => "",     # Alternate phone Country Code.
+        "phone_cc" => "",         # Telephone number Country Code. Required
+        "lang_pref" => "en",      # Language Code as per ISO. Required
+        "fax" => "",
+        "fax_cc" => "",           # Fax country code.
+        "mobile" => "",
+        "mobile_cc" => "",        # Mobile country code.
+      }
+      values = values.merge!(params)
       if validate(values)
         url = construct_url(values, "create")
         # Typhoeus::Request.post(url)
       else
-        raise "Kaboon!!!"
+        raise "Validation failed."
       end
     end
 
-    def update(kwargs={})
+    def update(params={})
+      # Example of use: Customer.update({"username" => "tyler@google.com","passwd" => "tambi5en5","name" => "Tyler","address_line_1" => "my casa", "company" => "my company.corb", "city" => "Lisa", "state" => "CH", "country" => "CU", "zipcode" => "11155", "phone_cc" => "53", "phone" => "531531","customer_id"=> "8940466"})
+      # Note: we cannot only pass the values we want to change, but all.
+      values = {
+        "auth_userid" => @@auth_userid,
+        "auth_password" => @@auth_password,
+        "username" => "",         # Should be an email address, required
+        "passwd" => "",           # Should be alphanumeric with a minimum of 8 and maximum of 16 characters. Required
+        "name" => "",             # Required
+        "company" => "",          # Required
+        "address_line_1" => "",   # Required
+        "address_line_2" => "",
+        "address_line_3" => "",
+        "city" => "",             # Required
+        "state" => "",            # Required
+        "country" => "",          # Country Code as per ISO 3166-1 alpha-2.
+        "zipcode" => "",          # Required
+        "phone" => "",            # Required
+        "alt_phone" => "",
+        "alt_phone_cc" => "",     # Alternate phone Country Code.
+        "phone_cc" => "",         # Telephone number Country Code. Required
+        "lang_pref" => "en",      # Language Code as per ISO. Required
+        "fax" => "",
+        "fax_cc" => "",           # Fax country code.
+        "mobile" => "",
+        "mobile_cc" => "",        # Mobile country code.
+        "customer_id" => "",      # Required
+      }
+      values = values.merge!(params)
+      if validate(values)
+        url = construct_url(values, "update")
+        Typhoeus::Request.post(url)
+      else
+        raise "Validation failed."
+      end
+
     end
   end
 end
