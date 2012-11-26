8e01b9af4d39bc6355bc18b1fff06c3f673cfff6
Damián Rodríguez Rivero <damianrr@gmail.com>
2012-11-26 09:30:47 -0500

    fixing http methods on domain.rb

--- lib/models/domain.rb
+++ lib/models/domain.rb
@@ -7,17 +7,15 @@ class Domain
 
     extend ResellerClubMethods
 
-    [{"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "available.json"},
-     {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "idn-available.json"},
-     {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "suggest-names.json"},
+    [{"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "available.json"},
+     {"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "idn-available.json"},
+     {"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "suggest-names.json"},
      {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "register.json"},
      {"values" => {"domain-name" => ""},"http_method" => "get", "validate" => lambda {|v| true}, "url" => "validate-transfer.json"},
      {"values" => {"domain-name" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "transfer.json"},
-     {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "eu/transfer.json"},
-     {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "trade.json"},
+     # {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "eu/transfer.json"},
+     # {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "trade.json"},
      {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "renew.json"},
-
-     # Check the http method and parameters from here also test
      {"values" => {"no-of-records" => "10", "page-no" => "1"}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "search.json"},
      {"values" => {"customer_id" => ""}, "http_method" => "get","validate" => lambda {|v| true}, "url" => "customer-default-ns.json"},
      {"values" => {"domain_name" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "orderid.json"},
@@ -30,18 +28,18 @@ class Domain
      {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "modify-contact.json"},
      {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "modify-privacy-protection.json"},
      {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "modify-auth-code.json"},
-     {"values" => {"order_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "enable-theft-protection.json"},
-     {"values" => {"order_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "disable-theft-protection.json"},
+     {"values" => {"order_id" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "enable-theft-protection.json"},
+     {"values" => {"order_id" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "disable-theft-protection.json"},
      {"values" => {"order_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "locks.json"},
      {"values" => {"order_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "tel/cth-details.json"},
      {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "tel/modify-whois-pref.json"},
-     {"values" => {"order_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "resend-rfa.json"},
-     {"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "uk/release.json"},
-     {"values" => {"order_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "cancel-transfer.json"},
+     {"values" => {"order_id" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "resend-rfa.json"},
+     {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "uk/release.json"},
+     {"values" => {"order_id" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "cancel-transfer.json"},
      {"values" => {"order_id" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "delete.json"},
      {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "restore.json"},
      {"values" => {"order_id" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "de/recheck-ns.json"},
-     {"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "dotxxx/association-details.json"},
+     {"values" => {}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "dotxxx/association-details.json"},
     ].each { |p| build_method p }
 
   end
